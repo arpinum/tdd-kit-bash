357af501dccb9ed61261f60bd679f1c0319c8248
Michael Borde <michael.borde@gmail.com>
2015-04-12 18:39:09 +0200

    Initial commit

---
+++ sources/fizzbuzz.sh
@@ -0,0 +1,34 @@
+fizzbuzz__enumerate() {
+  local i
+  for i in {1..100}; do
+    printf "%s " "$(_fizzbuzz__get_value_for_number ${i})"
+  done
+}
+
+_fizzbuzz__get_value_for_number() {
+  local number=$1
+  local result="$(_fizzbuzz__print_value_if_multiple "Fizz" ${number} 3)"
+  result+="$(_fizzbuzz__print_value_if_multiple "Buzz" ${number} 5)"
+  _fizzbuzz__print_value_of_default_if_empty "${result}" ${number}
+}
+
+_fizzbuzz__print_value_if_multiple() {
+  local value=$1
+  local number=$2
+  local divisor=$3
+  _fizzbuzz__number_is_multiple ${number} ${divisor} \
+    && printf "${value}" \
+    || printf ""
+}
+
+_fizzbuzz__number_is_multiple() {
+  (( $1 % $2 == 0 ))
+}
+
+_fizzbuzz__print_value_of_default_if_empty() {
+  local value="$1"
+  local default="$2"
+  [[ -n "${value}" ]] \
+    && printf "${value}" \
+    || printf "${default}"
+}
---
+++ tests/fizzbuzz_test.sh
@@ -0,0 +1,35 @@
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.."; pwd)"
+source "${ROOT_DIR}/sources/fizzbuzz.sh"
+
+global_setup() {
+  _enumeration=($(fizzbuzz__enumerate))
+}
+
+fizzbuzz_should_enumerate_100_elements() {
+  assertion__equal 100 ${#_enumeration[@]}
+}
+
+fizzbuzz_should_enumerate_fizz_for_3() {
+  _assert_value_for_number 3 "Fizz"
+}
+
+fizzbuzz_should_enumerate_fizz_for_all_multiples_of_3() {
+  _assert_value_for_number 6 "Fizz"
+}
+
+fizzbuzz_should_enumerate_buzz_for_5() {
+  _assert_value_for_number 5 "Buzz"
+}
+
+fizzbuzz_should_enumerate_buzz_for_15() {
+  _assert_value_for_number 15 "FizzBuzz"
+}
+
+_assert_value_for_number() {
+  assertion__equal "$2" "$(_get_value_for_number $1)"
+}
+
+_get_value_for_number() {
+  local array_index=$(($1 - 1))
+  printf "${_enumeration[${array_index}]}"
+}
